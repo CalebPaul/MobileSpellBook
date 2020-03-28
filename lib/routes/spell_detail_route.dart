68101b31f5834babb2a3e69100a258c9b2b03e3f
caleb <calebpaul21@gmail.com>
2020-03-28 15:31:39 -0700

    load multiple svgs for each spell damage type

--- lib/routes/spell_detail_route.dart
+++ lib/routes/spell_detail_route.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:spellbook/app.dart';
 import 'package:spellbook/data_models/spell_model.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:ui';
+import 'package:flutter_svg/avd.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class SpellDetailRoute extends StatelessWidget{
-
+class SpellDetailRoute extends StatelessWidget {
   final Spell spell;
+  final Widget svg = SvgPicture.asset('beaker.svg');
 
   SpellDetailRoute(this.spell);
 
@@ -12,16 +16,38 @@ class SpellDetailRoute extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(spell.name, style: AppTextStyles.header,)
-      ),
-      body: Container(
-        child: SafeArea(
+          title: Text(
+        spell.name,
+        style: AppTextStyles.header,
+      )),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
           child: Center(
-            child: Text(spell.description),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ...spell.buildDamageSvgPicture(),
+                  ],
+                ),
+                Text(
+                  spell.description,
+                  style: AppTextStyles.subtitle,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  spell.getDamageTypes().toString(),
+                  style: AppTextStyles.listTitle,
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+
 
 }
