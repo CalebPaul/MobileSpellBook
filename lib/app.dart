2318d5d7c717a0db878dcb633ddcff34cee4a2f4
caleb <calebpaul21@gmail.com>
2020-03-29 15:27:07 -0700

    add text styles

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,14 @@
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTextStyles {
-  static final header = GoogleFonts.specialElite(fontWeight: FontWeight.bold);
-  static final listTitle = GoogleFonts.specialElite(fontWeight: FontWeight.w500);
+  static final header2 = GoogleFonts.specialElite(fontWeight: FontWeight.bold);
+  static final header1 =
+      GoogleFonts.specialElite(fontWeight: FontWeight.bold, fontSize: 20);
+  static final listTitle =
+      GoogleFonts.specialElite(fontWeight: FontWeight.w500);
+  static final base = GoogleFonts.specialElite(fontSize: 18.0, color: Colors.black);
+  static final baseBold = GoogleFonts.specialElite(
+      fontWeight: FontWeight.bold, fontSize: 18.0, color: Colors.black);
   static final subtitle = GoogleFonts.specialElite(fontStyle: FontStyle.italic);
 }
